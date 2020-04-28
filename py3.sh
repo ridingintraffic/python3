678c4ffb09c5d78087f5b3acb8a87efa6f63fe39
Fettis <asdf>
2020-04-28 15:00:18 -0500

    asdfasdf

--- py3.sh
+++ py3.sh
@@ -6,6 +6,7 @@ IMAGE_NAME=${IMAGE_NAME:-py3}
 IMAGE_VERSION=${IMAGE_VERSION:-latest}
 [[ -n "${VARIABLE1:-}" ]] &&env+=(--env VARIABLE1="$VARIABLE1")
 [[ -n "${VARIABLE2:-}" ]] &&env+=(--env VARIABLE2="$VARIABLE2")
+[[ -d "${HOME}/.aws" ]] && volumes+=(--volume ${HOME}/.aws:/root/.aws)
 
 volumes+=(--volume ${PWD}/data:/data)
 
@@ -14,13 +15,17 @@ _flags="--interactive"
 
 [[ -n "${HTTP_PROXY:-}" ]] && env+=(--env HTTP_PROXY="$HTTP_PROXY")
 [[ -n "${HTTP_PROXY:-}" ]] && env+=(--env HTTPS_PROXY="$HTTP_PROXY")
+[[ -n "${http_proxy:-}" ]] && env+=(--env http_proxy="$http_proxy")
+[[ -n "${http_proxy:-}" ]] && env+=(--env https_proxy="$http_proxy")
+
 env+=(--env PYTHONHTTPSVERIFY=0)
 
 env+=(--env HTTP_PROXY="$http_proxy")
 env+=(--env HTTPS_PROXY="$http_proxy")
 env+=(--env PYTHONHTTPSVERIFY=0)
 env+=(--env CURL_CA_BUNDLE="")
-
+env+=(--env VAULT_TOKEN="$VAULT_TOKEN")
+env+=(--env AWS_PROFILE="saml")
 # env or volumes may be an empty array which bash treats as unset
 set +u
 exec docker run \
